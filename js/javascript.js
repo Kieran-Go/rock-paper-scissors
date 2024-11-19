b1ea14f83ea52c5ccf2d3075805ca7aef53b5a46
Kieran Gough <187461911+Kieran-Go@users.noreply.github.com>
2024-11-19 18:18:45 +1030

    Create two new functions to play the game
    The 'playGame()' and 'displayMatchResults()' functions were created, and
    the game is now fully functional.

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,20 @@
+// Prompts the player to pick between rock, paper or scissors
 function getHumanChoice(){
-    return prompt("Enter either 'rock', 'paper' or 'scissors'.").toUpperCase();
+    let choice;
+    // Use a while loop for input validation - it will continue to loop until a valid input is entered
+    while(true){
+        choice = prompt("Enter either 'rock', 'paper' or 'scissors'.").toUpperCase();
+        if(choice === "ROCK" || choice === "PAPER" || choice === "SCISSORS"){
+            break;
+        }
+        else{
+            alert(choice + " is not a valid choice! Please enter either 'rock', 'paper' or 'scissors'!")
+        }
+    }
+    return choice;
 }
 
+// The computer will pick randomly between 0, 1 and 2, which corrsponds to rock, paper and scissors respectively
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     switch(num){
@@ -17,55 +30,103 @@ function getComputerChoice(){
     }
 }
 
+// Finds and returns the result of the round by comparing the human and computer choices
 function playRound(humanChoice, computerChoice){
-    let win = false;
-    let draw = false;
+    let result = "Lose";
 
     if(humanChoice === computerChoice){
-        draw = true;
-        console.log("Draw! Both players used " + humanChoice.toLowerCase() + "!");
+        result = "Draw";
+        
     }
     else if(humanChoice === "ROCK"){
         if(computerChoice === "SCISSORS"){
-            win = true;
+            result = "Win";
         }
     }
     else if(humanChoice === "PAPER"){
         if(computerChoice === "ROCK"){
-            win = true;
+            result = "Win";
         }
     }
     else if(humanChoice === "SCISSORS"){
         if(computerChoice === "PAPER"){
-            win = true;
+            result = "Win";
         }
     }
 
-    if(!draw){
-        if(win){
-            console.log("You Win! Your " + humanChoice.toLowerCase() + " beat the computer's " + computerChoice.toLowerCase() + "!");
-            humanScore++;
-        }
-        else{
-            console.log("You lose! Your " + humanChoice.toLowerCase() + " lost to the computer's "
-            + computerChoice.toLowerCase() + "!");
-            computerScore++;
-        }
-    }
-
-    console.log("SCORE TALLEY: \n   Player: " + humanScore + "\n" + "   Computer: " + computerScore);
+    return result;
 }
 
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(rounds){
+    // Init game variables
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanChoice;
+    let computerChoice;
+    let roundResult;
+
+    // Use a for loop to play the game for all rounds
+    for (let i = 1; i < rounds+1; i++){
+        // Display the round number
+        console.log("--- ROUND " + i + " ---");
+
+        // Get the player's choice
+        humanChoice = getHumanChoice();
+
+        // Get the computer's choice
+        computerChoice = getComputerChoice();
+
+        // Get the round result by calling playRound
+        roundResult = playRound(humanChoice, computerChoice);
+
+        // Display the appropriate message and increment the score
+        switch(roundResult){
+            case "Win":{
+                console.log("You Win! Your " + humanChoice.toLowerCase() + " beat the computer's " + computerChoice.toLowerCase() + "!");
+                humanScore++;
+                break;
+            }
+            case "Lose":{
+                console.log("You lose! Your " + humanChoice.toLowerCase() + " lost to the computer's "
+                + computerChoice.toLowerCase() + "!");
+                computerScore++;
+                break;
+            }
+            case "Draw":{
+                console.log("Draw! Both players used " + humanChoice.toLowerCase() + "!");
+                break;
+            }
+        }
+
+        // Display the score
+        console.log("SCORE TALLEY: \n   Player: " + humanScore + "\n" + "   Computer: " + computerScore);
+    }
+
+    // Display match results
+    displayMatchResults(humanScore, computerScore);
+    
+}
+
+// Function that writes the match results to the console
+function displayMatchResults(humanScore, computerScore){
+    console.log("MATCH RESULTS:")
+    if(humanScore === computerScore){
+        console.log("Draw! Both players had " + humanScore + " points!");
+    }
+    else if(humanScore > computerScore){
+        console.log("You win! You scored " + humanScore + " points, while the computer scored " + computerScore + " points!");
+    }
+    else if(humanScore < computerScore){
+        console.log("You lose! You scored " + humanScore + " points, while the computer scored " + computerScore + " points!");
+    }
+}
 
 console.log("ROCK PAPER SCISSORS GAME!");
+// Enter a number as an argument for the playGame function to play for that many rounds
+playGame(5);
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
 
-playRound(humanChoice,computerChoice);
 
 
 
