026394ad3011ef1605fc320eb5d95866ddf4360b
Kieran Gough <187461911+Kieran-Go@users.noreply.github.com>
2024-11-19 17:25:22 +1030

    Write the get choice functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+function getHumanChoice(){
+    return prompt("Enter either 'rock', 'paper' or 'scissors'.").toUpperCase();
+}
+
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3);
+    switch(num){
+        case 0:{
+            return "ROCK";
+        }
+        case 1:{
+            return "PAPER";
+        }
+        case 2: {
+            return "SCISSORS";
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log("ROCK PAPER SCISSORS GAME!");
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+
+
+
+
