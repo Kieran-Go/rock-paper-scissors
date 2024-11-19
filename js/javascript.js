6313316b0aa0603b03c1e1b2399749899f175884
Kieran Gough <187461911+Kieran-Go@users.noreply.github.com>
2024-11-19 17:26:18 +1030

    Write the 'playRound()' function

--- js/javascript.js
+++ js/javascript.js
@@ -17,6 +17,46 @@ function getComputerChoice(){
     }
 }
 
+function playRound(humanChoice, computerChoice){
+    let win = false;
+    let draw = false;
+
+    if(humanChoice === computerChoice){
+        draw = true;
+        console.log("Draw! Both players used " + humanChoice.toLowerCase() + "!");
+    }
+    else if(humanChoice === "ROCK"){
+        if(computerChoice === "SCISSORS"){
+            win = true;
+        }
+    }
+    else if(humanChoice === "PAPER"){
+        if(computerChoice === "ROCK"){
+            win = true;
+        }
+    }
+    else if(humanChoice === "SCISSORS"){
+        if(computerChoice === "PAPER"){
+            win = true;
+        }
+    }
+
+    if(!draw){
+        if(win){
+            console.log("You Win! Your " + humanChoice.toLowerCase() + " beat the computer's " + computerChoice.toLowerCase() + "!");
+            humanScore++;
+        }
+        else{
+            console.log("You lose! Your " + humanChoice.toLowerCase() + " lost to the computer's "
+            + computerChoice.toLowerCase() + "!");
+            computerScore++;
+        }
+    }
+
+    console.log("SCORE TALLEY: \n   Player: " + humanScore + "\n" + "   Computer: " + computerScore);
+}
+
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -24,6 +64,8 @@ console.log("ROCK PAPER SCISSORS GAME!");
 
 let humanChoice = getHumanChoice();
 let computerChoice = getComputerChoice();
+
+playRound(humanChoice,computerChoice);
 
 
 
