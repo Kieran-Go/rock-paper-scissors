e9d57744e797332e96dbb959fbdebb0c41e8b124
Kieran Gough <187461911+Kieran-Go@users.noreply.github.com>
2024-11-21 10:58:01 +1030

    Announce winner once game is finished
    The winner is announced after the game is finished, or a draw is
    announced if the player and computer's scores are equal.
    Added a button that lets the user play another match.

--- js/javascript.js
+++ js/javascript.js
@@ -14,7 +14,7 @@ function getComputerChoice(){
     }
 }
 
-// Finds and returns the result of the round by comparing the human and computer choices
+// Determine the result of the round by comparing the human and computer choices
 function playRound(humanChoice){
     round++;
     let computerChoice = getComputerChoice();
@@ -42,30 +42,70 @@ function playRound(humanChoice){
 
 function displayResult(result, humanChoice, computerChoice){
 
-    // Reset content inside of resultContainer
+    // Clear result container
     resultContainer.innerHTML = "";
 
-    // Create result elements
-    const roundHeader = document.createElement("h3");
-    roundHeader.classList.add("roundHeader");
-    roundHeader.textContent = "Round " + round + " of " + MAX_ROUNDS;
-    roundHeader.setAttribute("style", "color: red");
-    resultContainer.appendChild(roundHeader);
+    if(round == MAX_ROUNDS){
+        announceWinner();
+    }
+    else{
+        // Create result elements
+        const roundHeader = document.createElement("h3");
+        roundHeader.textContent = "Round " + round + " of " + MAX_ROUNDS;
+        roundHeader.setAttribute("style", "color: red");
+        resultContainer.appendChild(roundHeader);
 
-    const roundResult = document.createElement("p");
-    roundResult.classList.add("roundResult");
-    roundResult.setAttribute("style", "white-space: pre");
-    roundResult.textContent = result + "!\r\n";
-    roundResult.textContent += "Player chose " + humanChoice + ", computer chose " + computerChoice;
-    resultContainer.appendChild(roundResult);
+        const roundResult = document.createElement("p");
+        roundResult.setAttribute("style", "white-space: pre");
+        roundResult.textContent = result + "!\r\n";
+        roundResult.textContent += "Player chose " + humanChoice + ", computer chose " + computerChoice;
+        resultContainer.appendChild(roundResult);
 
-    const scoreTalley = document.createElement("p");
-    scoreTalley.classList.add("scoreTalley");
-    scoreTalley.setAttribute("style", "white-space: pre");
-    scoreTalley.textContent = "--- SCORE ---\r\n";
-    scoreTalley.textContent += "Player: " + playerScore + "\r\n";
-    scoreTalley.textContent += "Computer: " + computerScore;
-    resultContainer.appendChild(scoreTalley);
+        const scoreTalley = document.createElement("p");
+        scoreTalley.setAttribute("style", "white-space: pre");
+        scoreTalley.textContent = "--- SCORE ---\r\n";
+        scoreTalley.textContent += "Player: " + playerScore + "\r\n";
+        scoreTalley.textContent += "Computer: " + computerScore;
+        resultContainer.appendChild(scoreTalley);
+    }
+}
+
+function announceWinner(){
+    const matchHeader = document.createElement("h3");
+    matchHeader.setAttribute("style", "color: red");
+    if(playerScore === computerScore){
+        matchHeader.textContent = "DRAW!";
+    }
+    else if(playerScore > computerScore){
+        matchHeader.textContent = "YOU WIN!";
+    }
+    else{
+        matchHeader.textContent = "YOU LOSE!";
+    }
+    resultContainer.appendChild(matchHeader);
+
+    const matchDetails = document.createElement("p");
+    matchDetails.setAttribute("style", "white-space: pre");
+    matchDetails.textContent = "Your score: " + playerScore + "\r\n";
+    matchDetails.textContent += "Computer score: " + computerScore;
+    resultContainer.appendChild(matchDetails);
+
+    const btnRestart = document.createElement("button");
+    btnRestart.textContent = "Play again?";
+    resultContainer.appendChild(btnRestart);
+
+    btnRestart.addEventListener("click", () => 
+    restartGame());
+}
+
+function restartGame(){
+    // Reset match data
+    round = 0;
+    playerScore = 0;
+    computerScore = 0;
+
+    // Clear result container
+    resultContainer.innerHTML = "";
 }
 
 const resultContainer = document.querySelector(".resultContainer");
@@ -79,10 +119,25 @@ const btnRock = document.querySelector("#btnRock");
 const btnPaper = document.querySelector("#btnPaper");
 const btnScissors = document.querySelector("#btnScissors");
 
-btnRock.addEventListener("click", () => 
-    playRound("ROCK"));
-btnPaper.addEventListener("click", () =>
-    playRound("PAPER"));
-btnScissors.addEventListener("click", () =>
-    playRound("SCISSORS"));
+btnRock.addEventListener("click", () => {
+    if(round < MAX_ROUNDS){
+        playRound("ROCK")
+    }
+    // Do nothing if game is over
+    return;
+});
+btnPaper.addEventListener("click", () => {
+    if (round < MAX_ROUNDS) {
+        playRound("PAPER");
+    }
+    // Do nothing if game is over
+    return;
+});
+btnScissors.addEventListener("click", () => {
+    if (round < MAX_ROUNDS) {
+        playRound("SCISSORS");
+    }
+    // Do nothing if game is over
+    return;
+});
 
